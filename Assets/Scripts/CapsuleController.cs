df26128350f567ac0c71a5ffa7a81115e35b1776
andrewshayne <andrewshayne@gmail.com>
2024-08-04 18:25:28 -0700

    first commit

---
+++ Assets/Scripts/CapsuleController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        GetComponent<Rigidbody>().velocity = moveVector * moveSpeed;
+    }
+}
